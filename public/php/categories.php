c1de1214340993ecd3ee77924685c3b11eb2c916
Purple4pur <purple4pur@gmail.com>
2020-02-03 23:52:52 +0800

    IMPROVE: use inner join for sql select

--- public/php/categories.php
+++ public/php/categories.php
@@ -10,32 +10,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql_get_post_info = "SELECT `post_id`, `author_id` FROM `post_info` WHERE `category_id` = ";
-$sql_get_post = "SELECT * FROM `post` WHERE `id` = ";
-$sql_get_author = "SELECT `name` FROM `author` WHERE `id` = ";
-$sql_get_tag_id = "SELECT `tag_id` FROM `post_tag` WHERE `post_id` = ";
-$sql_get_tag = "SELECT * FROM `tag` WHERE `id` = ";
+$sql_get_info_pre = "SELECT post_info.post_id AS id, post.time, post.title, post.content, author.name AS author FROM post_info, post, author WHERE post_info.category_id=";
+$sql_get_info_post = " AND post_info.post_id=post.id AND post_info.author_id=author.id ORDER BY post.time DESC";
+$sql_get_tag_pre = "SELECT tag.id, tag.name FROM post_tag, tag WHERE post_tag.post_id=";
+$sql_get_tag_post = " AND tag.id=post_tag.tag_id";
 
-$result_get_post_info = $conn->query($sql_get_post_info . $categoryID);
+$result_get_info = $conn->query($sql_get_info_pre . $categoryID . $sql_get_info_post);
 
-$arr = array();
-while ($row = $result_get_post_info->fetch_assoc()) {
-    $result_get_post = $conn->query($sql_get_post . $row["post_id"]);
-    $result_get_author = $conn->query($sql_get_author . $row["author_id"]);
-
-    $result = $result_get_post->fetch_assoc();
-    $result["author"] = $result_get_author->fetch_assoc()["name"];
-
-    $result_get_tag_id = $conn->query($sql_get_tag_id . $row["post_id"]);
+$arr = [];
+while ($row = $result_get_info->fetch_assoc()) {
+    $result_get_tag = $conn->query($sql_get_tag_pre . $row["id"] . $sql_get_tag_post);
     $tags = [];
-    while ($row2 = $result_get_tag_id->fetch_assoc()) {
-        $result_get_tag = $conn->query($sql_get_tag . $row2["tag_id"]);
-        $tag = $result_get_tag->fetch_assoc();
+    while ($tag = $result_get_tag->fetch_assoc()) {
         array_push($tags, $tag);
     }
-    $result["tags"] = $tags;
-
-    array_push($arr, $result);
+    
+    $row["tag"] = $tags;
+    array_push($arr, $row);
 }
 
 echo json_encode($arr, JSON_UNESCAPED_UNICODE);
